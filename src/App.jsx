3fe04c563d877b9aa5d31a6518fa288a52144eac
Ali Sadeghi <ali909392@gmail.com>
2026-06-06 23:44:48 +0330

    feat: add raw base files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HomePage from "./pages/HomePage";
+
+const App = () => {
+    return (
+        <div>
+            <h1>App</h1>
+
+            <HomePage />
+        </div>
+    );
+};
+
+export default App;
