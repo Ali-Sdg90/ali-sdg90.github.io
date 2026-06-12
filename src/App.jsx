380c23b3af8156bd3f3b132834210821cd338a72
Ali Sadeghi <ali909392@gmail.com>
2026-06-12 23:27:07 +0330

    feat: test setting the shelf background

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import DynamicBackground from "./components/DynamicBackground";
+import Shelf from "./components/Shelf";
 import HomePage from "./pages/HomePage";
 
 const App = () => {
     return (
-        <div>
+        <div className="page-style">
             <h1>App</h1>
 
             <DynamicBackground />
+
+            <Shelf />
 
             <HomePage />
         </div>
---
+++ src/components/Shelf.jsx
@@ -0,0 +1,18 @@
+const Shelf = () => {
+    return (
+        <div className="shelf-container">
+            <div className="shelf-section">
+                <div className="shelf-section-name">Section 1</div>
+
+                <div className="shelf-section-content">
+                    <div className="book"></div>
+                    <div className="book"></div>
+                    <div className="book"></div>
+                    <div className="book"></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Shelf;
